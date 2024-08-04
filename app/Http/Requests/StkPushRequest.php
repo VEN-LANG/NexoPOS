1c47e663f2ea0e90fff6af1783506bd940119e41
venomous-maker <morganokumu8@gmail.com>
2024-08-04 17:27:42 +0300

    Added messages to several required fields

--- app/Http/Requests/StkPushRequest.php
+++ app/Http/Requests/StkPushRequest.php
@@ -18,4 +18,15 @@ class StkPushRequest extends FormRequest
           'phoneNumber' => 'required|regex:/^\d{10,15}$/',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'phoneNumber.required' => __('The phone number field is required.'),
+            'phoneNumber.regex' => __('The phone number must be between 10 and 15 digits.'),
+            'amount.required' => __('The amount field is required.'),
+            'amount.numeric' => __('The amount must be a number.'),
+            'amount.min' => __('The amount must be at least 1.'),
+        ];
+    }
 }
