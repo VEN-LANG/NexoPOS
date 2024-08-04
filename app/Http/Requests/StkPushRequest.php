7c6c5c3fd201223dabea8c79d5eb6a353e9ebcc4
venomous-maker <morganokumu8@gmail.com>
2024-08-04 17:07:39 +0300

    Rules for Mpesa stkpush requests actions

---
+++ app/Http/Requests/StkPushRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StkPushRequest extends FormRequest
+{
+
+    public function authorize(){
+
+        return true;
+    }
+
+    public function rules(){
+        return [
+          'amount' => 'required|numeric|min:1',
+          'phoneNumber' => 'required|numeric|min:9',
+        ];
+    }
+}
