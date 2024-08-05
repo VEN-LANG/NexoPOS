51ffe0921294edf6c59bc388fb03f3fa22756cae
venomous-maker <morganokumu8@gmail.com>
2024-08-05 19:39:48 +0300

    Added mpesa specific route handler file

---
+++ routes/web/mpesa.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\MpesaTransactionsController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'mpesa'], function () {
+    Route::post("/callback", [MpesaTransactionsController::class, 'handleCallback']);
+});
