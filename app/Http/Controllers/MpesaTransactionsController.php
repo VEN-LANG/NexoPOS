04fa1960dcb534b4fdb18b6b2fc35104128ff8d3
kemboi22 <kemboielvis22@gmail.com>
2024-08-05 18:10:43 +0300

    feat(mpesa): handle Mpesa Call back

--- app/Http/Controllers/MpesaTransactionsController.php
+++ app/Http/Controllers/MpesaTransactionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\MpesaTransactions;
+use App\Services\MpesaService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -15,30 +16,8 @@ class MpesaTransactionsController extends Controller
 
     public function handleCallback(Request $request): void
     {
-        $data = $request->getContent();
-        $decodedData = json_decode($data, true);
-        Log::info("Callback data: ", $decodedData);
-        // Find Payment by transaction ID
-
-        try {
-            $payment = MpesaTransactions::query()
-                ->where('MerchantRequestID', $decodedData['Body']['stkCallback']['MerchantRequestID'])
-                ->where("CheckoutRequestID", $decodedData['Body']['stkCallback']['CheckoutRequestID'])
-                ->first();
-            Log::info("Payment: ", [
-                $payment
-            ]);
-            $paymentData = [
-                'transaction_code' => (string)$decodedData['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value'],
-                'phone_number' => (string)$decodedData['Body']['stkCallback']['CallbackMetadata']['Item'][3]['Value'],
-                'transaction_date' => \DateTime::createFromFormat('YmdHis', (string)$decodedData['Body']['stkCallback']['CallbackMetadata']['Item'][2]['Value']),
-                'transaction_amount' => (string)$decodedData['Body']['stkCallback']['CallbackMetadata']['Item'][0]['Value'],
-            ];
-            Log::info("Payment Data: ", $paymentData);
-            $payment?->update($paymentData);
-        } catch (\Exception $e) {
-            Log::error("Error: ", [$e->getMessage()]);
-        }
+        // Handle Mpesa Callback After customer has paid
+       (new MpesaService())->handleCallBack($request);
 
     }
 }
