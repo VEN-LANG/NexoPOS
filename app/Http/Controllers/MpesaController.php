a6c4f525dd3daf578d5e85014a60552871d4168a
kemboi22 <kemboielvis22@gmail.com>
2024-08-05 18:10:19 +0300

    feat(mpesa): added mpesa Call back

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StkPushRequest;
+use App\Models\MpesaTransactions;
+use App\Services\MpesaService;
 use Illuminate\Http\Request;
 
 class MpesaController extends Controller
@@ -11,9 +13,33 @@ class MpesaController extends Controller
      * @param StkPushRequest $request
      * @return string[]
      */
-    public function stkPush(StkPushRequest $request){
+    public function stkPush(StkPushRequest $request)
+    {
+        $mpesaService = new MpesaService();
+        $data = [
+            'amount' => $request->total_amount,
+            'phoneNumber' => $request->phoneNumber,
+            'transactionType' => 'CustomerPayBillOnline',
+            'accountReference' => "Mpesa payment for Order",
+            'transactionDesc' => 'Order Payment',
+        ];
+        $response = $mpesaService->sendStkPush($data);
+        $transaction = MpesaTransactions::query()
+            ->create([
+                'MerchantRequestID' => $response['response']['MerchantRequestID'],
+                'CheckoutRequestID' => $response['response']['CheckoutRequestID'],
+                'phone_number' => $request->phoneNumber,
+            ]);
+        if ($transaction)
+        {
+            return [
+                'status' => 'success',
+                'transaction' => $transaction
+            ];
+        }
         return [
-            'status' => 'success',
+            'status' => 'error',
+            'message' => 'Failed to create transaction'
         ];
     }
 
@@ -28,8 +54,25 @@ class MpesaController extends Controller
         ];
     }
 
-    public function stkResponse()
+    public function confirmation(Request $request)
     {
-        
+        // Check for mpesa Transaction with same number
+        $transaction = MpesaTransactions::query()
+            ->where("phone_number", $request->phoneNumber)
+            ->whereDoesntHave("order")
+            ->whereBetween("transaction_date", [now()->startOfDay(), now()->endOfDay()])
+            ->first();
+        if ($transaction)
+        {
+            // return Mpesa Transaction
+            return [
+                'status' => 'success',
+                'transaction' => $transaction
+            ];
+        }
+        return [
+            'status' => 'error',
+            'message' => 'No transaction found'
+        ];
     }
 }
