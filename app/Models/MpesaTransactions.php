65f4b2aa5a70ef909b432b10778d5c655f7d9ce6
kemboi22 <kemboielvis22@gmail.com>
2024-08-05 05:21:23 +0300

    feat(models): added mpesa transactions

---
+++ app/Models/MpesaTransactions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MpesaTransactions extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mpesa_transactions';
+
+    protected $fillable = [
+        'MerchantRequestID',
+        'CheckoutRequestID',
+        'transaction_code',
+        'phone_number',
+        'transaction_date',
+        'transaction_amount'
+    ];
+
+
+}
---
+++ database/migrations/2024_08_05_022834_create_mpesa_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mpesa_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('MerchantRequestID')->nullable();
+            $table->string('CheckoutRequestID')->nullable();
+            $table->string('transaction_code')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->date('transaction_date')->nullable();
+            $table->decimal('transaction_amount', 10, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mpesa_transactions');
+    }
+};
