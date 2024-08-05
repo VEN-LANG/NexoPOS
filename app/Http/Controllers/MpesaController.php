5b0bfcd28c92cc4331f6bfda2ecbb99588c00020
venomous-maker <morganokumu8@gmail.com>
2024-08-05 22:46:10 +0300

    Added amount to successful response

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -77,6 +77,7 @@ class MpesaController extends Controller
             return [
                 'success' => true,
                 'message' => __('Transaction Confirmed'),
+                'amount' => $transaction->transaction_amount,
                 'transaction' => $transaction
             ];
         }
