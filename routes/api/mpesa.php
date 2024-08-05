b059dcad933602402258826fa722f7a36101a24b
venomous-maker <morganokumu8@gmail.com>
2024-08-05 21:11:43 +0300

    Cahnged url from confirmation to sync payment

--- routes/api/mpesa.php
+++ routes/api/mpesa.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'mpesa'], function () {
     // Define your routes here
     Route::post('/stkpush', [MpesaController::class, 'stkPush'])->name('mpesa.stkpush');
-    Route::post("/confirmation", [MpesaController::class, 'confirmation'])->name('mpesa.confirmation');
+    Route::post("/syncPayment", [MpesaController::class, 'confirmation'])->name('mpesa.confirmation');
     // Add other routes as needed
 });
