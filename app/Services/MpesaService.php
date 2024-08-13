e643bd6746c5c3a47af98356515d960326e914ee
venomous-maker <morganokumu8+1@gmail.com>
2024-08-13 14:52:26 +0300

    Default gateway to safaricom till

--- app/Services/MpesaService.php
+++ app/Services/MpesaService.php
@@ -24,7 +24,7 @@ class MpesaService
         $this->callBackUrl = ns()->option->get('ns_accounting_mpesa_callback_url');
         $this->passKey = ns()->option->get('ns_accounting_mpesa_pass_key');
         $this->businessCode = ns()->option->get('ns_accounting_mpesa_business_code');
-        $this->transactionGateway = ns()->option->get('ns_accounting_mpesa_transaction_gateway');
+        $this->transactionGateway = ns()->option->get('ns_accounting_mpesa_transaction_gateway', 'safaricom_till');
 
         $this->mpesa = $this->mpesa->setCredentials($credentials['consumer_key'], $credentials['consumer_secret']);
     }
